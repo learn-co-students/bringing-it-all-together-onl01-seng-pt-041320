6b7e05d76c28b8227fcbba70ba527db6a7fe1223
vedransehovic <vedran.sehovic@gmail.com>
2020-06-15 15:10:43 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     attr_accessor :name, :breed
     attr_reader :id
 
-    def initialize(id: id=nil, name: name, breed: breed)
+    def initialize(id: nil, name: name, breed: breed)
         @id = id
         @name = name
         @breed = breed
@@ -27,8 +27,58 @@ class Dog
         DB[:conn].execute(sql)
     end
 
-    def new_from_db(row)
-        new_dog = self.new(row[0], row[1], row[2]) #new instance of a Dog
+    def save
+        if self.id
+          self.update
+        else
+          sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+          SQL
+          DB[:conn].execute(sql, self.name, self.breed)
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
+    end
+
+    def self.create( name: name, breed: breed)
+        student = Dog.new( name: name, breed: breed)
+        student.save
+        student
+      end
+    
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    def self.new_from_db(row)
+        new_dog = self.new(id: row[0], name: row[1], breed: row[2]) #new instance of a Dog
         new_dog
     end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM dogs WHERE name = ?"
+        result = DB[:conn].execute(sql, name)[0]
+        Dog.new(id: result[0], name: result[1], breed: result[2])
+    end
+
+    def self.find_by_id(id)
+        sql = "SELECT * FROM dogs WHERE id = ?"
+        result = DB[:conn].execute(sql, id)[0]
+        Dog.new(id: result[0], name: result[1], breed: result[2])
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+          dog_data = dog[0]
+          dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else
+          dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
 end
