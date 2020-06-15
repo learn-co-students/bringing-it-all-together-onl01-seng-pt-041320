d6318d1ee46db3213e867085fee4b80ec6e50939
vedransehovic <vedran.sehovic@gmail.com>
2020-06-15 14:23:59 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+class Dog
+
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id: id=nil, name: name, breed: breed)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql =  <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+          )
+          SQL
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL 
+            DROP TABLE dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def new_from_db
+
+    end
+end
