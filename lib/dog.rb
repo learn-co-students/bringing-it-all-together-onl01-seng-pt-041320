2636b697c5f20c8ddd960b04c2cd3264b2694e97
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-17 19:45:06 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,29 @@ class Dog
 
     def self.create_table
         sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
         SQL
+        DB[:conn].execute(sql)
     end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+        INSERT INTO dogs
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+
 end
