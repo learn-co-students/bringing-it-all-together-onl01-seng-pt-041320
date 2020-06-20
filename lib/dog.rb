b0724c1d8c66bd3f091ad2715d7674ecf37f0101
dnodolf1212 <dnodolf@gmail.com>
2020-06-20 16:25:24 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,6 +35,64 @@ class Dog
       self
   end 
 
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+  end 
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end 
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE id = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end  
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+    dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+    end 
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT * FROM dogs WHERE name = ?
+      SQL
+  
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
+    def update
+      sql = <<-SQL
+        UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+        SQL
+  
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+  
+  
+ 
+
 
 
 end 
